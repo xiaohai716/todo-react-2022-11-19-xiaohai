8629caf950549139a8d31c5ac6ba239428e749e1
xiaohai716 <71452303+xiaohai716@users.noreply.github.com>
2022-11-27 14:53:47 -0600

    try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Xiaohai Yang's ToDo App</h1>
+        <div class="screen">
+          <div class="app">
+            <h1>TODO APP</h1>
+            <form>
+              <input type="text" id = "todoText" placeholder="Create Your New Todo Task Here"></input>
+              <button type="submit">Create</button>
+            </form>
+            <ul class = "todolist" id = "todolist">
+              
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
